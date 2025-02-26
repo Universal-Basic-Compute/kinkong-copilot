d76748b9debde11ae037500b9873cf837b2d280f
Lesterpaintstheworld <reynolds.nicorr@gmail.com>
2025-02-26 01:28:21 +0100

    remove

--- dist/kinkong-copilot-v0.1.2/lib/marked-bridge.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('marked-ready', () => {
-  window.formatMarkdown = (text) => {
-    return marked.parse(text, {
-      breaks: true,
-      gfm: true,
-      sanitize: true
-    });
-  };
-});
